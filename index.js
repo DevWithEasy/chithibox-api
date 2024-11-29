d94b185b89e3b675a3ba0ec37c9ff5d2a9b40a66
DevWithEasy <devwitheasy@gmail.com>
2024-11-29 22:57:26 +0600

    socket

--- index.js
+++ index.js
@@ -1,20 +1,15 @@
 const express = require('express');
-const cors = require('cors');
+const http = require('http');
 const path = require('path');
 require("dotenv").config();
 const dbConnection = require("./config/dbConnection");
 const applyMidleware = require('./middleware/middlewares');
 const applyRouter = require('./routers/routers');
 const errorHandler = require('./middleware/errorHandler');
+const socketConnection = require('./config/socketConnection');
 
 const app = express();
-
-// Apply CORS middleware first
-app.use(cors({
-    origin: '*', // Allow all origins
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true // Include if you need to send cookies or authentication
-}));
+const server = http.createServer(app)
 
 // Serve static files
 app.use(express.static(path.join(__dirname, 'public')));
@@ -23,6 +18,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 applyMidleware(app);
 applyRouter(app);
 errorHandler(app);
+socketConnection(server)
 
 // Start the server
 app.listen(process.env.PORT || 8080, () => {
--- middleware/middlewares.js
+++ middleware/middlewares.js
@@ -3,11 +3,7 @@ const cors = require('cors')
 const express = require('express')
 
 const middlewares = [
-    // cors({
-    //     origin: '*',
-    //     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    //     credentials: true
-    // }),
+    cors(),
     morgan('dev'),
     express.urlencoded({extended: false}),
     express.json()
