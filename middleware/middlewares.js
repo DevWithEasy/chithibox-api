d51c705c24782fc6293956c38c4eccb37203bd4a
DevWithEasy <devwitheasy@gmail.com>
2024-11-29 21:59:58 +0600

    cors change

--- middleware/middlewares.js
+++ middleware/middlewares.js
@@ -3,7 +3,11 @@ const cors = require('cors')
 const express = require('express')
 
 const middlewares = [
-    cors(),
+    cors({
+        origin: '*',
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+        credentials: true
+    }),
     morgan('dev'),
     express.urlencoded({extended: false}),
     express.json()
